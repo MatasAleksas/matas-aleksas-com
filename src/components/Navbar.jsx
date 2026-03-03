17b496ec08473d98603a368b4a5a1a9553480361
MatasAleksas <matas.aleksas.usa@gmail.com>
2026-03-02 22:24:42 -0500

    Add shiny text to logo, add circle around contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,29 @@
 "use client";
+import ShinyText from './ShinyText';
 
 export default function Navbar() {
     return (
-        <nav className="fixed flex justify-between items-center mx-auto top-0 left-0 w-full z-50 px-6 py-4">
-            <div className="mx-13 flex gap-10 items-center">
+        <nav className="fixed upper-nav flex justify-center items-center mx-auto top-0 left-0 w-full z-50 px-3 py-4">
+            <div className="left-nav flex mx-13 gap-10 items-center">
                 <a className="text-orange nav-link" href='#about'>About</a>
                 <a className="text-orange nav-link" href='#professional'>Professional</a>
                 <a className="text-orange nav-link" href='#personal'>Personal</a>
             </div>
-            <a className="text-orange text-3xl font-bold flex-2 text-center mx-9" href='#'>Matas Aleksas</a>
-            <a className="text-orange flex-1 text-right mx-10" href='#contact'>Contact</a>
+            <a className="text-orange text-3xl font-bold mx-10 text-center" href='#'>
+                <ShinyText
+                    text="Matas Aleksas"
+                    speed={2}
+                    delay={0}
+                    color="#fb742d"
+                    shineColor="#fb542a"
+                    spread={120}
+                    direction="left"
+                    yoyo={false}
+                    pauseOnHover={false}
+                    disabled={false}
+                />
+            </a>
+            <a className="text-orange text-right mx-10 contact-btn" href='#contact'>Contact</a>
         </nav>
     );
 }
