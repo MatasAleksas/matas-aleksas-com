0438bd93199b6c7fffcda39c4ebca47b0e31c700
MatasAleksas <matas.aleksas.usa@gmail.com>
2026-03-12 12:33:26 -0400

    Add photos to repo and insert them in the website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -76,7 +76,7 @@ export default function Hero() {
                 </div>
 
                 <Image className={"hero-photo shrink-0"}
-                       src={"/images/profile-placeholder.webp"}
+                       src={"/images/hero.webp"}
                        width={250}
                        height={250}
                        alt={"Picture of author"}
